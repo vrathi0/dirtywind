2f91ffe855a978055c6abdea7a987ca984cc09eb
ivanhigueram <ivanhigueram@gmail.com>
2019-12-19 16:49:26 -0600

    Add parameter tibble function

---
+++ R/model_inputs.R
@@ -0,0 +1,33 @@
+#' Utility function to build a model parameter tibble in time 
+#' @param query PostgreSQL query string with unique identifier per source unit (lat, lon, and height).
+#' @param con A DBI Connection object (ideally from dbConnect()).
+#' @param duration duration of the HySPLIT trajectory model.
+#' @param start_date start date of modeling. See splitr documentation for more details.
+#' @param end_date end date of modeling. See splitr documentation for more details.
+#' @param daily_hours number of hours for modeling points within day. See splitr for more details.
+#' @param timedelta time units for modeling. Default is 1 month.
+
+
+#' @export model_inputs_unit
+model_inputs_unit  <- function(query, 
+                               con,
+                               timedelta='1 month',
+                               start_date,
+                               end_date,
+                               duration,
+                               daily_hours) {
+    
+
+    query_units <- dbGetQuery(conn=con,
+                              statement=query)
+    
+    query_parameters <- query %>% 
+        as_tibble(.) %>% 
+        tidyr::expand(tidyr::nesting(facility_id, latitude,longitude, facility_name, stack_height), 
+                      start_date = seq.Date(start_date, end_date, timedelta)) %>%
+        add_columns(duration = duration,
+                    daily_hours = list(daily_hours)
+    
+    return(query_parameters)
+
+
