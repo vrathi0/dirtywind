c6bf7fd4c92c1b83ad97691d6e0070532a409511
ivanhigueram <ivanhigueram@gmail.com>
2019-12-23 11:57:57 -0600

    RPostgres Driver nightmare
    This commit has a hotfix to deal with the schema name in the
    dbWriteTable function. Long story short, there's no unique way to add
    the schema name into the write table function. If we change drivers, we
    will have to change the relationship of table and schema. The
    documentation suggest the DBI::SQL function to unquote, but that has
    proven ineffective.
    
    We add a option into the engine driver to take the search path as the
    schema.

--- R/db_funcs.R
+++ R/db_funcs.R
@@ -1,20 +1,26 @@
 #' Function for SQL DB management
 
 #' @export send_output_db
-send_output_db  <- function(traj_model, con, table_name, schema, ...) {
+send_output_db  <- function(df, con, table_name, schema, ...) {
 
-    table_out  <- glue("{schema}.{table_name}")
-
-    if(inherits(model, 'traj_model')) {
-        db_out  <- dplyr::as.tbl(traj_model$traj_df)
-        dbWriteTable(conn=con,
-                     name=table_out,
-                     row.names=FALSE,
-                     append=TRUE,
-                     copy=TRUE,
-                     ...)
+    if(is.null(con)){
+        con <- pool::dbPool(RPostgres::Postgres(),
+                            user=Sys.getenv('USER'),
+                            password=Sys.getenv('PASSWORD'),
+                            host=Sys.getenv('HOST',),
+                            port=Sys.getenv('PORT'),
+                            dbname=Sys.getenv('DBNAME'),
+                            options=glue::glue("-c search_path={schema}"))
     }
 
+    #table_out  <- DBI::SQL(glue::glue('{schema}.{table_name}'))
+
+    message(glue::glue('Sending {dim(df)[1]} rows to dataabase'))
+    DBI::dbWriteTable(conn = con,
+                 name = table_name,
+                 value = df,
+                 row.names = FALSE,
+                 append = TRUE,
+                 copy = TRUE,
+                 ...)
 }
-
-
