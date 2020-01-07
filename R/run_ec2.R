01da69d2847ea0ab7a9de2fa96199154152fd001
ivanhigueram <ivanhigueram@gmail.com>
2020-01-06 23:18:46 -0700

    Add installation of private repo

--- R/run_ec2.R
+++ R/run_ec2.R
@@ -5,6 +5,7 @@
 #' @export make_cluster_ec2
 make_cluster_ec2  <- function(public_ip){
   ssh_private_key_file  <-  Sys.getenv('PEM_PATH')
+  github_pac  <-  Sys.getenv('PAC')
 
   cl_multi <- future::makeClusterPSOCK(
   workers = public_ip,
@@ -16,7 +17,8 @@ make_cluster_ec2  <- function(public_ip){
   ),
   rscript_args = c(
     "-e", shQuote("local({p <- Sys.getenv('R_LIBS_USER'); dir.create(p, recursive = TRUE, showWarnings = FALSE); .libPaths(p)})"),
-    "-e", shQuote("install.packages('furrr')")
+    "-e", shQuote("install.packages('furrr')"),
+    "-e", shQuote(glue::glue("devtools::install_github('cicala-projects/dirtywind', auth_token = {github_pac})"))
   ),
   dryrun = FALSE)
 
