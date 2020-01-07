56089c97292a68d489d80c2a052631db5de9fdd3
ivanhigueram <ivanhigueram@gmail.com>
2020-01-06 23:04:34 -0700

    solve issues with ec2 function socket

--- R/run_ec2.R
+++ R/run_ec2.R
@@ -4,31 +4,20 @@
 
 #' @export make_cluster_ec2
 make_cluster_ec2  <- function(public_ip){
-   cl_multi <- makeClusterPSOCK(
+  ssh_private_key_file  <-  Sys.getenv('PEM_PATH')
 
-  # Public IP number of EC2 instance
+  cl_multi <- future::makeClusterPSOCK(
   workers = public_ip,
-  ssh_private_key_file = Sys.getenv('PEM_PATH')
-
-  # User name (always 'ubuntu')
   user = "ubuntu",
-
-  # Use private SSH key registered with AWS
   rshopts = c(
     "-o", "StrictHostKeyChecking=no",
     "-o", "IdentitiesOnly=yes",
     "-i", ssh_private_key_file
   ),
-
-  # Set up .libPaths() for the 'ubuntu' user and
-  # install furrr
   rscript_args = c(
     "-e", shQuote("local({p <- Sys.getenv('R_LIBS_USER'); dir.create(p, recursive = TRUE, showWarnings = FALSE); .libPaths(p)})"),
     "-e", shQuote("install.packages('furrr')")
   ),
-
-  # Switch this to TRUE to see the code that is run on the workers without
-  # making the connection
   dryrun = FALSE)
 
   return(cl_multi)
