bdd315368a476490f9da2b1d85486c495963f611
ivanhigueram <ivanhigueram@gmail.com>
2020-01-10 17:08:56 -0600

    Add remotes to install splitr

--- R/make_cluster_ec2.R
+++ R/make_cluster_ec2.R
@@ -20,7 +20,7 @@ make_cluster_ec2  <- function(public_ip){
   ),
   rscript_args = c(
     "-e", shQuote("local({p <- Sys.getenv('R_LIBS_USER'); dir.create(p, recursive = TRUE, showWarnings = FALSE); .libPaths(p)})"),
-    "-e", shQuote("install.packages('devtools')"),
+    "-e", shQuote("install.packages(c('devtools', 'remotes')"),
     "-e", shQuote(glue::glue("devtools::install_github('cicala-projects/dirtywind', auth_token = '{github_pac}')")),
     "-e", shQuote("dirtywind::create_split_dirs(overwrite = TRUE)")
   ),
