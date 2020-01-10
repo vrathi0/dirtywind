0c3720d75d372b4b437f503863e3efd35701a599
ivanhigueram <ivanhigueram@gmail.com>
2020-01-10 16:41:40 -0600

    Add overwrite option to cluster creator function

--- R/make_cluster_ec2.R
+++ R/make_cluster_ec2.R
@@ -16,13 +16,15 @@ make_cluster_ec2  <- function(public_ip){
   rshopts = c(
     "-o", "StrictHostKeyChecking=no",
     "-o", "IdentitiesOnly=yes",
-    "-i", ssh_private_key_file
+    "-i", ssh_private_key_file,
+    "--verbose"
+
   ),
   rscript_args = c(
     "-e", shQuote("local({p <- Sys.getenv('R_LIBS_USER'); dir.create(p, recursive = TRUE, showWarnings = FALSE); .libPaths(p)})"),
     "-e", shQuote("install.packages('devtools')"),
     "-e", shQuote(glue::glue("devtools::install_github('cicala-projects/dirtywind', auth_token = '{github_pac}')")),
-    "-e", shQuote("dirtywind::create_split_dirs()")
+    "-e", shQuote("dirtywind::create_split_dirs(overwrite = TRUE)")
   ),
   dryrun = FALSE)
 
