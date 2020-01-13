1f547d6c1b91d704061c72391c0410794434b79a
ivanhigueram <ivanhigueram@gmail.com>
2020-01-13 15:34:38 -0600

    Play with globals

--- R/hysplit_trajectory_parallel_master.R
+++ R/hysplit_trajectory_parallel_master.R
@@ -206,7 +206,7 @@ hysplit_trajectory_parallel_master <- function(lat = 49.263,
     list_run_days <- days %>% as.character()
     
     # Initialize a vector for all filenames at this receptor
-    trajectory_files <- c()
+    trajectory_files <<- c()
     
     # Make loop with all run days using future/foreach
     future_map(
